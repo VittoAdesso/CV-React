975b2cf52cb56296473928fa8ecf0dfd6bb23699
Vittorio Adesso <adessovittorio8@gmail.com>
2022-01-18 00:03:17 +0100

    Mejoras y estilos

--- cv/src/components/Experience/Experience.jsx
+++ cv/src/components/Experience/Experience.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './Experience.css'
 
 const Experience = ( { experience }) => {
     return (
         <div>
-            <div className="experience card">
+            <div className="experience-card">
                 {experience.map((item) => {
                         return (
                             <div key={JSON.stringify(item)}>
-                            <p className="name">📕 {item.name}</p>
+                            <p className="name"> 💼 {item.name}</p>
                             <p>{item.where}</p>
                             <p>{item.date}</p>
                             </div>
--- cv/src/components/More/More.jsx
+++ cv/src/components/More/More.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './More.css'
 
-const More = () => {
+const More = ( { languages, habilities })  => {
     return (
         <div>
-            
+               <div className="more-card">
+                {languages.map((item) => {
+                        return (
+                            <div key={JSON.stringify(item)}>
+                            <p className="name">🌍🌎🌏 {item.language}</p>
+                            <p>✏️ {item.wrlevel}</p>
+                            <p>🤓 {item.splevel}</p>
+                            </div>
+                        );
+                })}
+            </div>
+
+            <div className="habilities-card">
+           { habilities.map((item) =>
+                <p>📌{item}</p>
+            )}
+              
+            </div>
+
         </div>
     )
 }
